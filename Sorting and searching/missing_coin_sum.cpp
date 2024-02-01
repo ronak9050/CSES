3a0d37a8fd689c3479376c15086ac513ee71b28d
Ronak Bhatiya <ronakcancode@gmail.com>
2024-02-02 01:33:17 +0530

    missing_coin_sum

--- Sorting and searching/missing_coin_sum.cpp
+++ Sorting and searching/missing_coin_sum.cpp
@@ -10,6 +10,14 @@ int main(){
     vector<ll> a(n);
     for(int i=0; i<n; i++) cin>>a[i];
     sort(a.begin(),a.end());
-    
+    ll sum=0,ans=1e15;
+    for(int i=0; i<n; i++){
+        if(sum+1<a[i]) ans=min(ans,sum+1);
+        else{
+            sum+=a[i];
+        } 
+    }
+    if(ans!=1e15) cout<<ans<<endl;
+    else cout<<sum+1<<endl;
     return 0;
 }
